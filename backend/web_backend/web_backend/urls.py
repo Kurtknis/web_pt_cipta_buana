019d1ca4325c8df068f8d32b2905cfab3c777977
Fathir <233756438+fathirgilangr@users.noreply.github.com>
2026-02-07 19:41:00 +0700

    Fix API url

--- backend/web_backend/web_backend/urls.py
+++ backend/web_backend/web_backend/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('', RedirectView.as_view(pattern_name='admin:index', permanent=False)),
+    path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
 ]
