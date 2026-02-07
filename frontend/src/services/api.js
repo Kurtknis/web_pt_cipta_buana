bbb8ba72b8bc3ece0ab068e144fbb9abfd0f58b8
Fathir <233756438+fathirgilangr@users.noreply.github.com>
2026-02-07 20:36:27 +0700

    Remove Debug

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -14,9 +14,6 @@ export const submitConsultation = async (formData) => {
       deskripsi: formData.description,
     };
 
-    console.log("API URL:", API_BASE_URL);
-    console.log("API Key:", API_KEY);
-
     const response = await fetch(`${API_BASE_URL}/konsultasi/`, {
       method: 'POST',
       headers: {
@@ -25,16 +22,6 @@ export const submitConsultation = async (formData) => {
       },
       body: JSON.stringify(payload),
     });
-
-    const text = await response.text();
-    console.log('Raw response:', text);
-    
-    let data = {};
-    try {
-      data = JSON.parse(rawText);
-    } catch (err) {
-      console.warn('Response was not valid JSON');
-    }
 
     if (!response.ok) {
       const errorData = await response.json();
