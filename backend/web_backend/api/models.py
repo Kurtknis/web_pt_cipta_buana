318ecc2a849c221f389ee0acd5c070dd4121403c
Fathir <233756438+fathirgilangr@users.noreply.github.com>
2026-02-17 17:35:34 +0700

    Add Addable Data - Add Konsultasi, Proyek, GambarProyek, Biaya, Klien, Kontak Models - Register New Models to Admin - Add phonenumberslite package to requirement.txt - Add phonenumbers_field to app in settings.py

--- backend/web_backend/api/models.py
+++ backend/web_backend/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 
@@ -14,8 +15,77 @@ class Konsultasi(models.Model):
     
     class Meta:
         verbose_name = 'Data Konsultasi'
-        verbose_name_plural = verbose_name
         ordering = ['-tanggal_pengiriman_form']
         
     def __str__(self):
-        return f"{self.nama} - {self.proyek}"
+        return f'{self.nama} - {self.proyek}'
+    
+class Gambar(models.Model):
+    gambar = models.ImageField(upload_to='gambar/')
+    alt = models.CharField(blank=True, null=True)
+    
+    class Meta:
+        verbose_name = 'Data Konsultasi'
+    
+    def save(self, *args, **kwargs):
+        if not self.alt:
+            self.alt = f'{self.gambar} Image'
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return f'Gambar {self.alt}'
+    
+class Proyek(models.Model):
+    judul = models.CharField(max_length=100)
+    lokasi = models.CharField(max_length=100, default='-')
+    tanggal = models.IntegerField(blank=True, null=True, default='-')
+    deskripsi = models.TextField(blank=True, null=True, default='-')
+    harga = models.CharField(blank=True, null=True, default='-')
+    durasi = models.CharField(blank=True, null=True, default='-')
+    
+    class Meta:
+        verbose_name = 'Daftar Proyek'
+        
+    def __str__(self):
+        return self.judul
+    
+class GambarProyek(models.Model):
+    proyek = models.ForeignKey(Proyek, on_delete=models.CASCADE, related_name='gambar')
+    gambar = models.ImageField(upload_to='gambar_proyek/')
+    
+    class Meta:
+        verbose_name = 'Galeri Proyek'
+        
+    def __str__(self):
+        return f'Gambar {self.proyek}'
+    
+class Biaya(models.Model):
+    luas = models.CharField(max_length=100)
+    harga = models.CharField(max_length=50)
+    deskripsi = models.TextField(blank=True, null=True, default='-')
+    
+    class Meta:
+        verbose_name = 'Daftar Harga per meter persegi'
+        
+    def __str__(self):
+        return f'{self.luas} - {self.harga}'
+    
+class Klien(models.Model):
+    nama = models.CharField(max_length=100)
+    
+    class Meta:
+        verbose_name = 'Daftar Klien'
+        
+    def __str__(self):
+        return self.nama
+    
+class Kontak(models.Model):
+    telepon = PhoneNumberField(max_length=15, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    alamat = models.CharField(blank=True, null=True, max_length=200)
+    
+    class Meta:
+        verbose_name = 'Kontak Perusahaan'
+        
+    def __str__(self):
+        return f'{self.telepon} - {self.email} - {self.alamat}'
