67bd6c9c77aa65fd38d6be9d5c375d27c300fce5
Fathir <233756438+fathirgilangr@users.noreply.github.com>
2026-02-20 23:10:04 +0700

    Fix admin.py

--- backend/web_backend/api/admin.py
+++ backend/web_backend/api/admin.py
@@ -1,23 +1,5 @@
 from django.contrib import admin
-from .models import Consultation, Project, HomePortfolio, HomeComparisonImage, ImageProject, Furniture, Price, Client, Contact
-
+from .models import Consultation
 # Register your models here.
 
 admin.site.register(Consultation)
-
-
-class ImageProjectInline(admin.TabularInline):
-    model = ImageProject
-    extra = 1
-
-@admin.register(Project)
-class ProjectAdmin(admin.ModelAdmin):
-    inlines = [ImageProjectInline]
-
-admin.site.register(HomePortfolio)
-admin.site.register(HomeComparisonImage)
-admin.site.register(ImageProject)
-admin.site.register(Price)
-admin.site.register(Client)
-admin.site.register(Contact)
-admin.site.register(Furniture)
--- backend/web_backend/api_page/admin.py
+++ backend/web_backend/api_page/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Project, HomePortfolio, HomeComparisonImage, ImageProject, Furniture, Price, Client, Contact
 
 # Register your models here.
+
+class ImageProjectInline(admin.TabularInline):
+    model = ImageProject
+    extra = 1
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [ImageProjectInline]
+
+admin.site.register(HomePortfolio)
+admin.site.register(HomeComparisonImage)
+admin.site.register(ImageProject)
+admin.site.register(Price)
+admin.site.register(Client)
+admin.site.register(Contact)
+admin.site.register(Furniture)
